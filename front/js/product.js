c091c45a05c50f0a5bb47e59368270a8223c89bf
Xemn <spysschaert.pro@gmail.com>
2022-03-29 23:01:21 +0200

    Fistly : Correct an error of variable line 4 change currenr by current and line 6 replace the argument research by currentURLParams | Secondly : Request my api with a GET method and display the information i want about my product into the DOM

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,41 @@
 /* Récupération Valeur ID : */
 
 // On recherche les différents paramètres de notre url courrante :
-const currenrURLParams = window.location.search;
+const currentURLParams = window.location.search;
 // On structure notre variable de type string en objet URLSearchParams :
-const urlParams = new URLSearchParams(researchParams);
+const urlParams = new URLSearchParams(currentURLParams);
 // On récupère la valeur de notre paramètre id à l'aide de la méthode get :
 const id = urlParams.get("id");
 
 const url = "http://localhost:3000/api/products";
+
+function getProduct() {
+  fetch(url + "/" + id)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (product) {
+      const imageOfProduct = document.createElement("img");
+      imageOfProduct.setAttribute("src", `${product.imageUrl}`);
+      imageOfProduct.setAttribute("alt", `${product.altTxt}`);
+      document.getElementsByClassName("item__img").appendChild(imageOfProduct);
+
+      document.getElementById("title").textContent = product.name;
+      document.getElementById("price").textContent = product.price;
+      document.getElementById("description").textContent = product.description;
+
+      for (let color in product.colors) {
+        const option = document.createElement("option");
+        option.setAttribute("value", color);
+        option.textContent = color;
+        document.getElementById("colors").appendChild(option);
+      }
+    })
+    .catch(function (err) {
+      alert("Un problème est survenu");
+    });
+}
+
+getProduct();
